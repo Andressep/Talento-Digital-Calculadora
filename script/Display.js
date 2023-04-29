baaa4e20618aa3002d10d102ee7a0a6df01f40eb
Andres Sepulveda <bodyweightforce@gmail.com>
2023-04-29 16:02:05 -0400

    Calculadora funcional

---
+++ script/Display.js
@@ -0,0 +1,55 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorAnterior = displayValorAnterior;
+        this.displayValorActual = displayValorActual;
+        this.calculadora = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorAnterior = '';
+        this.valorActual = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '%',
+            multiplicar: 'x',
+            restar: '-', 
+        }
+    }
+
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0, -1);
+        this.imprimirValores();
+    }
+
+    borrarTodo() {
+        this.valorAnterior = '';
+        this.valorActual = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        if (numero === '.' && this.valorActual.includes('.')) return 
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if (isNaN(valorAnterior) || isNaN(valorActual)) return 
+        this.valorActual= this.calculadora[this.tipoOperacion](valorAnterior, valorActual);
+    }
+}
